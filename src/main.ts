18fc161144762db5beb1be13abf0c74b1517e6a4
udityaprakash <udityaprakash983@gmail.com>
2024-05-31 12:30:51 +0530

    ready for first deploy #01

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Server running on port:${port}`);
 }
 bootstrap();
