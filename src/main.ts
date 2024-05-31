a72ee82ced3b78b7e6dd0dbec4259ed5ccce0f09
udityaprakash <udityaprakash983@gmail.com>
2024-05-31 12:21:24 +0530

    ready for first deploy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(helmet());
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Server running on port:${port}`);
 }
 bootstrap();
