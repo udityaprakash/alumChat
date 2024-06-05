347612b50fc327a2d0e73f39a99c875184db4a55
udityaprakash <udityaprakash983@gmail.com>
2024-06-05 09:37:26 +0530

    added a secret in dotenv

--- src/modules/auth/services/jwt-strategy.service.ts
+++ src/modules/auth/services/jwt-strategy.service.ts
@@ -4,13 +4,15 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 // import { JwtPayload } from '../interfaces/jwtpayload.interface';
 import { AuthService } from './oauth.service';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly authService: AuthService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: '456',
+      secretOrKey: process.env.JWT_SECRET,
     });
   }
 
